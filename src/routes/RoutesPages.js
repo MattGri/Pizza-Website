1a9e2bbb55e97fd357bbd58660c77dc9bdf98988
Mateusz <mateusz.grzybowski98@gmail.com>
2022-04-22 12:35:01 +0200

    Created Contact Page

--- src/routes/RoutesPages.js
+++ src/routes/RoutesPages.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../components/HomePage/HomePage';
 import ProductPage from '../components/ProductPage/ProductPage';
+import ContactPage from '../components/ContactPage/ContactPage';
 
 const RoutesPages = () => {
   return (
     <Routes>
       <Route path="/" exact element={<HomePage />} />
       <Route path="/product" exact element={<ProductPage />} />
+      <Route path="/contact" exact element={<ContactPage />} />
     </Routes>
   );
 };
