ee49206661e8c9bc78f12f094ca84aac37500ea5
Mateusz <mateusz.grzybowski98@gmail.com>
2022-04-29 11:13:42 +0200

    Added home page title

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,8 +1,13 @@
 import Navigation from './Navigation';
 import MainPizza from './MainPizza';
 import Footer from './Footer';
+import { useEffect } from 'react';
 
 const HomePage = () => {
+  useEffect(() => {
+    document.title = 'Home Page';
+  }, []);
+
   return (
     <>
       <Navigation />
